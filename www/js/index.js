b2b1746e1b3e8ead9559626dad6749862daab108
GTRebbeca <rebbecagt@gmail.com>
2018-08-02 12:52:57 -0500

    prueba sin blink en ios

--- www/js/index.js
+++ www/js/index.js
@@ -20,14 +20,14 @@ var app = {
 
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        //this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        //document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
